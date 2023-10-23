c748b54c5f5d2b192009eb9d2391936b82fe2f5f
KexySerJ90 <exserj9011@gmail.com>
2023-10-23 21:36:03 +0300

    add slugs

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sitewoman project.
+URL configuration for sitewomen project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -11,8 +11,8 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the includes() function: from django.urls import includes, path
-    2. Add a URL to urlpatterns:  path('blog/', includes('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -21,8 +21,8 @@ from women.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('women.urls'))
+    path('', include('women.urls')),
 ]
 
 
-handler404=page_not_found
+handler404 = page_not_found
--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,7 +1,9 @@
 from django import template
 import women.views as views
 
-register=template.Library()
+
+register = template.Library()
+
 
 @register.simple_tag(name='getcats')
 def get_categories():
@@ -10,5 +12,5 @@ def get_categories():
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(cat_selected=0):
-    cats=views.cats_db
-    return {'cats':cats,'cat_selected':cat_selected}
+    cats = views.cats_db
+    return {'cats': cats, 'cat_selected': cat_selected}
